98c907bb67e4d9e91ffa501474d887a8d20fbb36
pnumin <pnumin@pusan.ac.kr>
2024-08-20 22:58:15 +0900

    주사위

---
+++ 04/04.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+  //요소 가져오기
+  const img = document.querySelector('img') ;
+  const bt1 = document.getElementById('bt1') ;
+
+  //이벤트 감지
+  bt1.addEventListener('click', ()=>{
+    let n = Math.floor(Math.random() * 6) + 1 ; //1~6까지 랜덤수 생성
+    img.setAttribute('src', `../img/${n}.png`) ;
+    img.setAttribute('alt', n) ;
+  });
+});
