7fa1db1c5cf65830d242ba7b8c06af880c4fc145
pnumin <pnumin@pusan.ac.kr>
2024-08-21 10:34:09 +0900

    자바스크립트 주사위게임

--- 05/05.js
+++ 05/05.js
@@ -1,12 +1,28 @@
 document.addEventListener('DOMContentLoaded', ()=>{
   //요소 가져오기
-  const img = document.querySelector('img') ;
-  const bt1 = document.getElementById('bt1') ;
+  const imgCom = document.querySelector('#com') ;
+  const imgUser = document.querySelector('#user') ; 
+  const bts = document.querySelectorAll('button') ;
+  const msg = document.querySelector('#msg') ;
 
   //이벤트 감지
-  bt1.addEventListener('click', ()=>{
-    let n = Math.floor(Math.random() * 6) + 1 ; //1~6까지 랜덤수 생성
-    img.setAttribute('src', `../img/${n}.png`) ;
-    img.setAttribute('alt', n) ;
-  });
+  for(let bt of bts) {
+    bt.addEventListener('click', ()=>{
+      let nUser = bt.textContent.charAt(0) ;
+      // console.log(nUser);
+      let nCom = Math.floor(Math.random()*6) + 1 ;
+
+      //이미지 변경
+      imgCom.setAttribute('src', `../img/${nCom}.png`);
+      imgUser.setAttribute('src', `../img/${nUser}.png`);
+
+      //결과확인
+      if (nCom == nUser) {
+        msg.innerHTML = '<h3 id="ho">맞음</h3>';
+      }
+      else {
+        msg.innerHTML = '<h3 id="hx">틀림</h3>';
+      }
+    }) ;
+  }
 });
